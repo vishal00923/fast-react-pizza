a0a4372838441c0afbd5949330bbfa26dd02b81a
Vishal Chaurasia <vishalbg.mz@gmail.com>
2025-07-03 00:12:20 +0530

    add AppLayout

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,27 @@
-export const App = () => {
-  return <div>Hello, Vite!</div>;
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import Home from './ui/Home';
+import Menu from './features/menu/Menu';
+import Cart from './features/cart/Cart';
+import CreateOrder from './features/order/CreateOrder';
+import Order from './features/order/Order';
+import AppLayout from './ui/AppLayout';
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      { path: '/', element: <Home /> },
+      { path: '/menu', element: <Menu /> },
+      { path: '/cart', element: <Cart /> },
+      { path: '/order/new', element: <CreateOrder /> },
+      { path: '/order/:orderId', element: <Order /> },
+    ],
+  },
+]);
+
+const App = () => {
+  return <RouterProvider router={router} />;
 };
+
+export default App;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  return (
+    <header>
+      <Link to='/'>Fast React Pizza</Link>
+
+      <p>Name</p>
+    </header>
+  );
+};
+
+export default Header;
