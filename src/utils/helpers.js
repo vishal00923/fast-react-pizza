06acd41acedd17794d2d07623885ba7f28c80a19
Vishal Chaurasia <vishalbg.mz@gmail.com>
2025-08-03 17:22:25 +0530

    Update helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,16 @@
 export const formatCurrency = (price) => {
   return new Intl.NumberFormat('en', {
     style: 'currency',
-    currency: 'INR',
+    currency: 'USD',
   }).format(price);
 };
 
-export const formatDate = (date) => {
+export const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+  if (isNaN(date.getTime())) {
+    return 'Invalid date';
+  }
+
   return new Intl.DateTimeFormat('en', {
     day: 'numeric',
     month: 'short',
@@ -14,8 +19,12 @@ export const formatDate = (date) => {
   }).format(date);
 };
 
-export const calcMinutesLeft = (date) => {
+export const calcMinutesLeft = (dateStr) => {
   const d1 = new Date().getTime();
-  const d2 = new Date(date).getTime();
+  const d2 = new Date(dateStr).getTime();
   return Math.round((d2 - d1) / 60000);
 };
+
+export const isValidPhoneNumber = (str) =>
+  // https://uibakery.io/regex-library/phone-number
+  /^\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}$/.test(str);
